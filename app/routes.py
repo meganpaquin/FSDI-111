e92f3452d488b9be88907f35408a491ada77ade6
Megan Paquin <106716810+meganpaquin@users.noreply.github.com>
2022-09-12 20:44:45 -0400

    add html files

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.get("/")
 def index():
-    return "<h1>Hello, world!</h1>"
+    return render_template("index.html")
+
+@app.get("/about")
+def about():
+    me = {
+        "first_name" : "Megan",
+        "last_name" : "Paquin",
+        "hobbies" : "Crafts",
+        "bio" : "enter bio here"
+    }
+
+    return render_template("about.html")
